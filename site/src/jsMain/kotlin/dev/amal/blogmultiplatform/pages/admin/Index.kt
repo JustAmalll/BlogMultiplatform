f4d424bdddbd40cc0f63b40e718c8d6599ff4bde
JustAmalll <amalkrasavchik@gmail.com>
2024-03-10 22:25:43 +0800

    Create AddButton

--- site/src/jsMain/kotlin/dev/amal/blogmultiplatform/pages/admin/Index.kt
+++ site/src/jsMain/kotlin/dev/amal/blogmultiplatform/pages/admin/Index.kt
@@ -1,9 +1,36 @@
 package dev.amal.blogmultiplatform.pages.admin
 
 import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.css.Cursor
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
+import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
+import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.cursor
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.height
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.onClick
+import com.varabyte.kobweb.compose.ui.modifiers.position
+import com.varabyte.kobweb.compose.ui.modifiers.size
+import com.varabyte.kobweb.compose.ui.styleModifier
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.core.rememberPageContext
+import com.varabyte.kobweb.silk.components.icons.fa.FaPlus
+import com.varabyte.kobweb.silk.components.icons.fa.IconSize
+import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
 import dev.amal.blogmultiplatform.components.AdminPageLayout
+import dev.amal.blogmultiplatform.models.JsTheme
+import dev.amal.blogmultiplatform.navigation.Screen
 import dev.amal.blogmultiplatform.util.isUserLoggedIn
+import org.jetbrains.compose.web.css.Position
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.vh
 
 @Page
 @Composable
@@ -16,6 +43,37 @@ fun HomePage() {
 @Composable
 fun HomeScreen() {
     AdminPageLayout {
+        AddButton()
+    }
+}
 
+@Composable
+private fun AddButton() {
+    val breakpoint = rememberBreakpoint()
+    val context = rememberPageContext()
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .position(Position.Fixed)
+            .styleModifier { property("pointer-events", "none") },
+        contentAlignment = Alignment.BottomEnd
+    ) {
+        Box(
+            modifier = Modifier
+                .margin(all = if (breakpoint > Breakpoint.MD) 40.px else 20.px)
+                .size(size = if (breakpoint > Breakpoint.MD) 80.px else 50.px)
+                .backgroundColor(JsTheme.Primary.rgb)
+                .borderRadius(r = 14.px)
+                .cursor(Cursor.Pointer)
+                .onClick { context.router.navigateTo(Screen.AdminCreate.route) }
+                .styleModifier { property("pointer-events", "auto") },
+            contentAlignment = Alignment.Center
+        ) {
+            FaPlus(
+                modifier = Modifier.color(Colors.White),
+                size = IconSize.LG
+            )
+        }
     }
 }
