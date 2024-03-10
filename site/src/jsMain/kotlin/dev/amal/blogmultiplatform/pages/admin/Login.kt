23da18c202e4de869598eee831e1607e2ef98807
JustAmalll <dev.justamal@gmail.com>
2024-03-10 15:36:14 +0800

    Design Login Screen

---
+++ site/src/jsMain/kotlin/dev/amal/blogmultiplatform/pages/admin/Login.kt
@@ -0,0 +1,121 @@
+package dev.amal.blogmultiplatform.pages.admin
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import com.varabyte.kobweb.compose.css.Cursor
+import com.varabyte.kobweb.compose.css.FontWeight
+import com.varabyte.kobweb.compose.css.TextAlign
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
+import com.varabyte.kobweb.compose.ui.modifiers.border
+import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
+import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.cursor
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.compose.ui.modifiers.fontWeight
+import com.varabyte.kobweb.compose.ui.modifiers.height
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.padding
+import com.varabyte.kobweb.compose.ui.modifiers.textAlign
+import com.varabyte.kobweb.compose.ui.modifiers.width
+import com.varabyte.kobweb.compose.ui.toAttrs
+import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.core.rememberPageContext
+import com.varabyte.kobweb.silk.components.forms.Input
+import com.varabyte.kobweb.silk.components.forms.UnstyledInputVariant
+import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.text.SpanText
+import dev.amal.blogmultiplatform.models.JsTheme
+import org.jetbrains.compose.web.attributes.InputType
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.Button
+
+@Page
+@Composable
+fun LoginScreen() {
+    val scope = rememberCoroutineScope()
+    val context = rememberPageContext()
+    val errorText by remember { mutableStateOf(" ") }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(leftRight = 50.px, top = 80.px, bottom = 24.px)
+                .backgroundColor(JsTheme.LightGray.rgb),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier
+                    .margin(bottom = 50.px)
+                    .width(100.px),
+                src = "/logo.svg"
+            )
+            Input(
+                modifier = Modifier
+                    .margin(bottom = 12.px)
+                    .width(350.px)
+                    .height(54.px)
+                    .borderRadius(r = 4.px)
+                    .padding(leftRight = 20.px)
+                    .backgroundColor(Colors.White)
+                    .fontSize(14.px),
+                type = InputType.Text,
+                value = "",
+                onValueChanged = {},
+                placeholder = "Username",
+                variant = UnstyledInputVariant
+            )
+            Input(
+                modifier = Modifier
+                    .margin(bottom = 24.px)
+                    .width(350.px)
+                    .height(54.px)
+                    .borderRadius(r = 4.px)
+                    .padding(leftRight = 20.px)
+                    .backgroundColor(Colors.White)
+                    .fontSize(14.px),
+                type = InputType.Password,
+                value = "",
+                onValueChanged = {},
+                placeholder = "Password",
+                variant = UnstyledInputVariant
+            )
+            Button(
+                attrs = Modifier
+                    .margin(bottom = 24.px)
+                    .width(350.px)
+                    .height(54.px)
+                    .backgroundColor(JsTheme.Primary.rgb)
+                    .color(Colors.White)
+                    .borderRadius(r = 4.px)
+                    .fontWeight(FontWeight.Medium)
+                    .fontSize(16.px)
+                    .border(width = 0.px)
+                    .cursor(Cursor.Pointer)
+                    .toAttrs()
+            ) {
+                SpanText(text = "Sign in")
+            }
+            SpanText(
+                modifier = Modifier
+                    .width(350.px)
+                    .color(Colors.Red)
+                    .textAlign(TextAlign.Center),
+                text = errorText
+            )
+        }
+    }
+}
---
+++ site/src/jsMain/kotlin/dev/amal/blogmultiplatform/util/Constants.kt
@@ -0,0 +1,4 @@
+package dev.amal.blogmultiplatform.util
+
+object Constants {
+}
